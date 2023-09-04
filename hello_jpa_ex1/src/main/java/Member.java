9d44ba0ea3184ffc4dd83f3c774a2ccdff5b2cab
yunju2 <sikka2034@gmail.com>
2023-09-04 20:31:20 +0900

    first jpa study

---
+++ hello_jpa_ex1/src/main/java/Member.java
@@ -0,0 +1,30 @@
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name",nullable = false)
+    private String username;
+
+    public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
